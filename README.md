ef2156c51226d017d7756e5ebcb89dc2f3521bbf
Christian Asch <casch@cenat.ac.cr>
2023-03-20 11:46:22 -0600

    Added code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rofi-language-switcher
-Simple language switcher using Rofi
+
+Simple language switcher using Rofi, based on [empris](https://github.com/madprops/empris).  
+Simply change the `languages.txt` file with the languages and layouts you want available.
---
+++ language_switcher.py
@@ -0,0 +1,42 @@
+#!/bin/python
+import os
+import csv
+
+class Rofi:
+    def __init__(self, args=None):
+        self.args = args
+
+    def select(self, prompt, options, selected):
+        items = "\n".join(options)
+        ans = os.popen(f"echo '{items}' | rofi -dmenu -p '{prompt}' -format i \
+            -selected-row {selected} -me-select-entry ''\
+            -me-accept-entry 'MousePrimary' {self.args}").read().strip()
+
+        if ans == "":
+            return -1
+
+        return int(ans)
+
+def show_menu(languages):
+    rofi = Rofi()
+
+    options = []
+    for lang in languages:
+        options.append(lang[1])
+
+    selected = 0
+    selected = rofi.select("Select layout", options, selected)
+
+    if selected != -1:
+        os.popen(f'setxkbmap {languages[selected][1]}')
+
+if (__name__ == "__main__"):
+    languages = []
+    index = 0
+    with open('languages.txt', 'r', encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            languages.append((row['language'], row['code']))
+            index += 1
+
+    show_menu(languages)
